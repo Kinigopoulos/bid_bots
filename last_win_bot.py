73dc8e0477ed328245448dfef17ab267fc42d660
ChrysoWasTaken <36905084+ChrysoWasTaken@users.noreply.github.com>
2020-12-28 22:30:47 +0200

    Update last_win_bot.py
    Row 13 ήταν if self.money<= win.amount που είναι λάθος για αυτο που θέλει το bot και δεν θα τρεξει ποτε. το max(0,win_amount) το έβαλα επειδή το win_amount στον πρωτο γύρο είναι -1 και θα πετάξει ερρορ αν επιστραφει απλά το win_amount

--- last_win_bot.py
+++ last_win_bot.py
@@ -9,6 +9,6 @@ class LastWinBot():
         self.money += 500
         if winner == 0:
             self.money -= win_amount
-        if self.money <= win_amount:
-            return win_amount
+        if self.money >= win_amount:
+            return max(0,win_amount)
         return 0
